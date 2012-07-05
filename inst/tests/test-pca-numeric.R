a13b233640a0c7762cbedf03b2a13d4be015507e
Jean-Olivier Irisson <irisson@normalesup.org>
2012-07-05 20:51:07 +0200

    Add commeents and TODOs for tests

--- inst/tests/test-pca-numeric.R
+++ inst/tests/test-pca-numeric.R
@@ -4,14 +4,18 @@ context("Comparability of PCA functions")
 set.seed(123)
 d <- data.frame(a=runif(5), b=runif(5), c=runif(5))
 
+# PCA with stats::prcomp
 pcaS <- prcomp(d, scale=TRUE)
 obsS <- fortify(pcaS, type="observations")
 varS <- fortify(pcaS, type="variables")
 
+# PCA with FactoMineR::PCA
 library("FactoMineR")
 pcaF <- PCA(d, scale.unit=TRUE, graph=F)
 obsF <- fortify(pcaF, type="observations")
 varF <- fortify(pcaF, type="variables")
+
+# TODO add ade4
 
 test_that("eigenvalues are equal", {
   eigS <- pcaS$sdev^2
@@ -19,3 +23,5 @@ test_that("eigenvalues are equal", {
 
   expect_equal(eigS, eigF)
 })
+
+# TODO add some more tests, comparing the values returned by the various fortify methods. Ideally, they should all be equivalent.
