38d70aaf1071583b0e40320e292925354cd23754
Jean-Olivier Irisson <irisson@normalesup.org>
2015-03-03 11:48:19 -0500

    New documentation syntax from Roxygen 4.0.2

--- man-roxygen/scaling.R
+++
@@ -1,3 +0,0 @@
-#'
-#' @param scaling how to scale scores. Can be "none", "obs", "var", "both" (or synonyms of "obs" and "var", as for the argument \code{type}). Can also be a integer from 0 to 3, which designs a scaling as in the order above (0="none", 1="obs", etc.). Scaling observations makes the distances in the plot of observations approximations of the euclidan distance in the original space. Scaling variables relates the angle between variables to the correlations between them (right angle = no correlation). Scaling of observation or variables is by the square root of the eigenvalues. Scaling both is by the fourth root of the eigenvalues.
-#'
--- man-roxygen/type.R
+++
@@ -1,3 +0,0 @@
-#'
-#' @param type the type of element to be extracted: either "observations", "individuals", "sites", "lines" (which are all synonyms) or "variables", "descriptors", "species", "columns" (which are, again, synonyms)
-#'
