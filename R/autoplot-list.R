8b6aedadc4133fcdfbbbb009aa9805bb824c5b70
Jean-Olivier Irisson <irisson@normalesup.org>
2015-10-14 21:39:31 +0200

    Better documentation of autoplot.list

--- R/autoplot-list.R
+++ R/autoplot-list.R
@@ -3,10 +3,10 @@
 #' Similar to \code{\link[graphics]{image}}
 #'
 #' @export
-#' @param model list with components x, y and z
+#' @param object list with components x, y and z
 #' @param data not used by this method
+#' @param mapping a call to aes() specifying additional mappings between variables and plot aesthetics; by default, positions in x and y are mapped to the x and y coordinates and fill is mapped to the values contained in the z component in the list
 #' @param ... not used by this method
-#' @importFrom reshape2 melt
 #' @examples
 #' x <- seq(0, 1, by=0.1)
 #' y <- seq(0, 2, by=0.1)
@@ -15,7 +15,7 @@
 #' image(d)
 #' autoplot(d)
 #' autoplot(d, mapping=aes(alpha=x))
-autoplot.list <- function(object, mapping=aes(), ...) {
+autoplot.list <- function(object, data=NULL, mapping=aes(), ...) {
   d <- fortify(object)
   
   # add user mappings
