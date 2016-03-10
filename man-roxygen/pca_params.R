ca3f6e41deb387aea6bf796188862fc49c2255b9
Jean-Olivier Irisson <irisson@normalesup.org>
2016-03-10 14:42:04 +0100

    Remove empty lines which cause problems with roxygen

--- man-roxygen/pca_params.R
+++ man-roxygen/pca_params.R
@@ -1,4 +1,3 @@
-#'
 #' @param data the original dataset, to be concatenated to the output when extracting observations. When \code{NULL}, the default, data will be extracted from the PCA object when it contains it (i.e. not for \code{\link{prcomp}})
 #'
 #' @param type the elements to consider: either "observations", "individuals", "sites", "lines" (which are all synonyms) or "variables", "descriptors", "species", "columns" (which are, again, synonyms)
@@ -7,4 +6,3 @@
 #'
 #' @param scaling how to scale scores. Can be "none", "obs", "var", "both" (or synonyms of "obs" and "var", as for the argument \code{type}). Can also be a integer from 0 to 3, which designs a scaling as in the order above (0="none", 1="obs", etc.). Scaling observations makes the distances in the plot of observations approximations of the euclidan distance in the original space. Scaling variables relates the angle between variables to the correlations between them (right angle = no correlation). Scaling of observation or variables is by the square root of the eigenvalues. Scaling both is by the fourth root of the eigenvalues.
 #' TODO add a note about auto scaling
-#'
--- man-roxygen/pca_seealso.R
+++ man-roxygen/pca_seealso.R
@@ -1,3 +1,1 @@
-#'
 #' @seealso \code{\link{prcomp}} in package \code{stats}, \code{\link[FactoMineR:PCA]{PCA}} in package \code{factoMineR}, \code{\link[ade4:dudi.pca]{dudi.pca}} in package \code{ade4}, \code{\link[vegan:rda]{rda}} in package \code{vegan},   \code{\link[pcaMethods:pca]{pca}} in package \code{pcaMethods} from bioconductor.
-#'
