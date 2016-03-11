aa35c1428db33154defa3ad6ce63ee1f36d543ee
Jean-Olivier Irisson <irisson@normalesup.org>
2016-03-11 16:42:13 +0100

    Add extraction of scores for all PCAs, including scaling
    More modular and cleaner than what I had!

---
+++ R/ordination_scores.R
@@ -0,0 +1,304 @@
+#' Extract scores from an ordination object
+#'
+#' @inheritParams eigenvalues
+#'
+#' @param type the scores (i.e. coordinates in the new space) to extract: either "rows", "lines", "observations", "objects", "individuals", "sites" (which are all treated as synonyms) or "columns", "variables", "descriptors", "species" (which are, again, synonyms). All can be abbreviated. By default, scores of rows are returned.
+#'
+#' @param scaling scaling for the scores. Can be
+#' \describe{
+#'   \item{"none" (or 0)}{for raw scores,}
+#'   \item{"rows" (or 1, or a synonym of "rows")}{to scale row scores by the eigenvalues,}
+#'   \item{"column" (or 2, or a synonym of "column")}{to scale column scores by the eigenvalues,}
+#'   \item{"both" (or 3)}{to scale both row and column scores.}
+#' }
+#' By default, scaling is adapted to the type of scores extracted (scaling 1 for row scores and scaling 2 for column scores).
+#' 
+#' @details
+#' Scaling of scores follows the conventions of package \code{vegan}. In summary, except for scaling 0, scores are all multiplied by a constant:
+#'	\deqn{c = \sqrt[4]{(n-1) \times \sum{eig}}}{c = sqrt(sqrt((n-1) * sum(eigenvalues)))}
+#' where \eqn{n} is the number of active rows in the ordination and \eqn{eig} are the eigenvalues (see function \code{\link{eigenvalues}}).
+#' In addition, for scaling 1 (resp. 2), row scores (resp. column scores) are multiplied by:
+#' \deqn{\sqrt{\frac{eig}{\sum{eig}}}}{sqrt(eig/sum(eig))}
+#' For scaling 3, both row and column scores are multiplied by:
+#' \deqn{\sqrt[4]{\frac{eig}{\sum{eig}}}}{sqrt(sqrt(eig/sum(eig)))}
+#' For details and justification, see \code{vignette("decision-vegan")}.
+#' 
+#' @examples
+#' # Principal Component Analysis
+#' pca <- prcomp(USArrests, scale=TRUE)
+#' head(scores(pca))
+#' head(scores(pca, type="columns"))
+#' head(scores(pca, type="columns", scaling=0))
+#' head(scores(pca, type="columns", scaling=3))
+#'
+#' sc <- scores(pca)
+#' plot(sc$PC1, sc$PC2, asp=1)
+#' text(sc$PC1, sc$PC2, label=sc$label, adj=c(-0.1,0.5))
+#'
+#' if (require("FactoMineR")) {
+#'   head(scores(PCA(USArrests, graph=F)))
+#' }
+#' if (require("vegan")) {
+#'   head(scores(rda(USArrests, scale=TRUE)))
+#' }
+#' if (require("ade4")) {
+#'   head(scores(dudi.pca(USArrests, scannf=FALSE, nf=4)))
+#' }
+#' if (require("pcaMethods")) {
+#'   head(scores(pca(USArrests, scale="uv", nPcs=4)))
+#' }
+#'
+#'
+#' @export
+scores <- function(x, type="rows", scaling=type, ...) {
+  UseMethod("scores")
+}
+
+scores_ <- function(x, type="rows", scaling=type, ...) {
+  # TODO add selection of axes here?
+  # check arguments for the type of scores and scaling
+  type <- match_type(type)
+  scaling <- match_scaling(scaling, type)
+  
+  # compute eigenvalues and number of active rows to:
+  # - convert scores computed by the various packages to a common "scale 0"
+  # - scale scores afterwards
+  eig <- eigenvalues(x)
+  nr <- nr(x)
+  
+  # get and scale scores
+  if (type == "row") {
+    scores <- row_scores(x, eig, nr)
+    scaled <- scale_row_scores(scores, eig, nr, scaling)
+  } else if (type == "col") {
+    scores <- col_scores(x, eig)
+    scaled <- scale_col_scores(scores, eig, nr, scaling)
+  }
+
+  # convert to a nicely formatted data.frame
+  scaled <- as.data.frame(scaled)
+  # homogenise column names
+  nc <- length(eig)
+  names(scaled)[1:nc] <- paste0("PC", 1:nc)
+  # get labels as a proper data.frame column
+  scaled$label <- row.names(scaled)
+  row.names(scaled) <- NULL
+  
+  return(scaled)
+}
+
+#' @name scores
+#' @export
+scores.prcomp <- scores_
+#' @name scores
+#' @export
+scores.PCA <- scores_
+#' @name scores
+#' @export
+scores.rda <- scores_
+#' @name scores
+#' @export
+scores.pca <- scores_
+#' @name scores
+#' @export
+scores.pcaRes <- scores_
+
+
+## Utility functions for scores extraction and scaling ----
+
+
+# Determine the type of scores to extract from an ordination object
+# This defines synonyms to allow the vocabulary from different packages to co-exist
+match_type <- function(type, ...) {
+  # define synonyms
+  row_types <- c("rows", "lines", "observations", "objects", "individuals", "sites")
+  col_types <- c("columns", "variables", "descriptors", "species")
+
+  # allow abbreviation
+  type <- match.arg(type, c(row_types, col_types, ...), several.ok=FALSE)
+
+  # reduce synonyms
+  type[type %in% row_types] <- "row"
+  type[type %in% col_types] <- "col"
+
+  return(type)
+}
+
+# Determine the type scaling for scores extracted from an ordination object
+# This uses the same synonyms
+match_scaling <- function(scaling, type) {
+  # otherwise select a type of scaling
+  if (is.numeric(scaling)) {
+    # scaling can be a number, in a way compatible with most of the code out there (vegan in particular)
+    if (! scaling %in% 0:3) {
+      stop("Scaling should be 0, 1, 2, or 3")
+    }
+    scaling <- c("none", "row", "col", "both")[scaling+1]
+
+  } else if (is.character(scaling)) {
+    # or a character string, specifying in which "direction" to scale
+    scaling <- match_type(scaling, c("none", "both"))
+  
+  } else {
+    stop("Scaling should be a number between 0 and 3 or a character string")
+  }
+  
+  return(scaling)
+}
+
+
+# Get number of active observations (data rows)
+# generic
+nr <- function(x) { UseMethod("nr") }
+# define methods
+nr.prcomp <- function(x) { nrow(x$x) }
+nr.PCA    <- function(x) { nrow(x$ind$coord) }
+nr.rda    <- function(x) { nrow(x$CA$u) }
+nr.pca    <- function(x) { nrow(x$li) }
+nr.pcaRes <- function(x) { nrow(x@scores) }
+
+
+
+# Extract scale 0 scores
+
+# generics to dispatch to methods
+col_scores <- function(x, ...) { UseMethod("col_scores") }
+row_scores <- function(x, ...) { UseMethod("row_scores") }
+
+# convert to scale 0
+row_unscale <- function(x, eig, nr) { t(t(x) / sqrt((nr - 1) * eig)) } 
+row_unscale_n <- function(x, eig, nr) { t(t(x) / sqrt(nr * eig)) }
+
+# define methods
+row_scores.prcomp <- function(x, eig, nr) { row_unscale(x$x, eig, nr) }
+row_scores.PCA <- function(x, eig, nr) { row_unscale_n(x$ind$coord, eig, nr) } # TODO deal with supplementary
+row_scores.rda <- function(x, ...) { x$CA$u }
+row_scores.pca <- function(x, eig, nr) { row_unscale_n(x$li, eig, nr) }
+row_scores.pcaRes <- function(x, eig, nr) { row_unscale(x@scores, eig, nr) }
+
+# convert to scale 0
+col_unscale <- function(x, eig) { t(t(x) / sqrt(eig)) } 
+
+# define methods
+col_scores.prcomp <- function(x, ...) { x$rotation }
+col_scores.PCA <- function(x, eig) { col_unscale(x$var$coord, eig) } # TODO deal with supplementary
+col_scores.rda <- function(x, ...) { x$CA$v }
+col_scores.pca <- function(x, ...) { x$c1 }
+col_scores.pcaRes <- function(x, ...) { x@loadings }
+
+
+# Scale scores
+# Conventions are those of vegan. See vignette("decision-vegan") for details.
+scale_row_scores <- function(x, eig, nr, scaling="none", ...) {
+  # define scaling factors
+  prop_eig_2 <- sqrt(eig/sum(eig))
+  prop_eig_4 <- sqrt(prop_eig_2)
+  const <- ((nr-1)*sum(eig))^(1/4)
+  
+  # perform scaling
+  switch(scaling,
+    none = x,
+    row = t(t(x) * (prop_eig_2 * const)),
+    col = x * const,
+    both = t(t(x) * (prop_eig_4 * const))
+  )
+}
+
+scale_col_scores <- function(x, eig, nr, scaling="none", ...) {
+  # define scaling factors
+  prop_eig_2 <- sqrt(eig/sum(eig))
+  prop_eig_4 <- sqrt(prop_eig_2)
+  const <- ((nr-1)*sum(eig))^(1/4)
+  
+  # perform scaling
+  switch(scaling,
+    none = x,
+    row = x * const,
+    col = t(t(x) * (prop_eig_2 * const)),
+    both = t(t(x) * (prop_eig_4 * const))
+  )
+}
+
+# # Extraction of supplementary data in FactoMineR
+# col_scores.PCA <- function(x, eig, ...) {
+#   res <- data.frame()
+#   kind <- c()
+#   for (cat in c("var", "quanti.sup")) {
+#     if (!is.null(x[[cat]])) {
+#       X <- as.data.frame(t(t(x[[cat]]$coord) / sqrt(eig)))
+#       res <- rbind(res, X)
+#       kind <- c(kind, rep(cat, nrow(X)))
+#     }
+#   }
+#   attr(res, "kind") <- kind
+#   return(res)
+# }
+#
+# row_scores.PCA <- function(x, eig, ...) {
+#   res <- data.frame()
+#   kind <- c()
+#   for (cat in c("ind", "ind.sup", "quali.sup")) {
+#     if (!is.null(x[[cat]])) {
+#       X <- as.data.frame(t(t(x[[cat]]$coord) / sqrt(eig)) * sqrt(1 / nrow(x[[cat]]$coord)))
+#       res <- rbind(res, X)
+#       kind <- c(kind, rep(cat, nrow(X)))
+#     }
+#   }
+#   kind <- gsub("ind", "obs", kind, fixed=TRUE)
+#   attr(res, "kind") <- kind
+#   return(res)
+# }
+#
+# # Comparison between packages
+# # scaling 0
+# # scaling 1, rows
+# # scaling 2, cols
+# # scaling 3, both
+# # see vignette("decision-vegan")
+# xS <- prcomp(USArrests, scale=TRUE)
+# xV <- vegan::rda(USArrests, scale=TRUE)
+# xF <- FactoMineR::PCA(USArrests, graph=F)
+# xA <- ade4::dudi.pca(USArrests, scannf=FALSE, nf=4)
+# xM <- pcaMethods::pca(USArrests, scale="uv", nPcs=4)
+#
+# # VEGAN
+# n <- nrow(scores(xV, "r"))
+# (eig <- eigenvalues(xV))
+# (prop_eig_2 <- sqrt(eig/sum(eig)))
+# (prop_eig_4 <- sqrt(prop_eig_2))
+# (const <- ((n-1)*sum(eig))^(1/4))
+# (prop_eig_2 * const)
+#
+# rs0 <- vegan:::scores.rda(xV, 1:4, display = c("sites"), scaling=0)
+# rs1 <- vegan:::scores.rda(xV, 1:4, display = c("sites"), scaling=1)
+# rs2 <- vegan:::scores.rda(xV, 1:4, display = c("sites"), scaling=2)
+# rs3 <- vegan:::scores.rda(xV, 1:4, display = c("sites"), scaling=3)
+#
+# all.equal((rs1 / rs0)[1,], prop_eig_2 * const, check.attributes=F)
+# all.equal((rs2 / rs0)[1,], rep(const, 4), check.attributes=F)
+# all.equal((rs3 / rs0)[1,], prop_eig_4 * const, check.attributes=F)
+#
+# cs0 <- vegan:::scores.rda(xV, 1:4, display = c("species"), scaling=0)
+# cs1 <- vegan:::scores.rda(xV, 1:4, display = c("species"), scaling=1)
+# cs2 <- vegan:::scores.rda(xV, 1:4, display = c("species"), scaling=2)
+# cs3 <- vegan:::scores.rda(xV, 1:4, display = c("species"), scaling=3)
+#
+# all.equal((cs1 / cs0)[1,], rep(const, 4), check.attributes=F)
+# all.equal((cs2 / cs0)[1,], prop_eig_2 * const, check.attributes=F)
+# all.equal((cs3 / cs0)[1,], prop_eig_4 * const, check.attributes=F)
+#
+# # FACTOMINER
+# rs <- xF$ind$coord
+# cs <- xF$var$coord
+# rs0 <- scores(xF, "r")
+# cs0 <- scores(xF, "c")
+#
+# eig <- eigenvalues(xF)
+# n <- nrow(rs)
+#
+# all.equal((rs / rs0)[1,], sqrt(eig * n), check.attributes=F)
+# all.equal((cs / cs0)[1,], sqrt(eig), check.attributes=F)
+#
+#
+# # ADE4
+
